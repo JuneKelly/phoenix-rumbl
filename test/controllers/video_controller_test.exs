cd12b904b98f3149e0d5afe28a147332ebda4291
Shane Kilkelly <shane@kilkelly.me>
2016-07-25 21:27:22 +0100

    auth test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -20,4 +20,24 @@ defmodule Rumbl.VideoControllerTest do
     end)
   end
 
+  # setup %{conn: conn} = config do
+  #   if username = config[:login_as] do
+  #     user = insert_user(username: username)
+  #     conn = assign(conn, :current_user, user)
+  #     {:ok, conn: conn, user: user}
+  #   else
+  #     :ok
+  #   end
+  # end
+
+  # @tag login_as: "max"
+  # test "lists all user's videos on index", %{conn: conn, user: user} do
+  #   user_video = insert_video(user, title: "wat")
+  #   other_video = insert_video(insert_user(username: "other"), title: "cat")
+
+  #   conn = get conn, video_path(conn, :index)
+  #   assert html_response(conn, 200) =~ ~r/Listing Videos/
+  #   assert String.contains?(conn.resp_body, user_video.title)
+  #   refute String.contains?(conn.resp_body, other_video.title)
+  # end
 end
